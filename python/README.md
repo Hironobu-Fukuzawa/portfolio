4457456bcc3eff9913f8ebb4211aa60a2ecff279
H Alan Fukuzawa <h.f@eichinoMacBook-Air.local>
2022-09-01 16:51:52 +0800

    Fixed bugs about distance function

---
+++ python/README.md
@@ -0,0 +1,21 @@
+# Find Nearst Location
+
+# NUS optional homework
+FILE: find_nearest_restaurants.py
+DESIGN: Return all locations which satifify the distance which is less than the particular distance.
+Input: (50, 50)
+Output: [{(44, 64): 'A'}, {(47, 57): 'P'}, {(48, 46): 'X'}, {(44, 66): 'h'}]
+
+# Customized Program
+FILE: find_nearest_three_restaurants.py
+DESIGN: Return all locations which satifify the distance which is less than the particular distance.
+Input: (50, 50)
+Output: [{(44, 64): 'A'}, {(47, 57): 'P'}, {(48, 46): 'X'}, {(44, 66): 'h'}]
+
+
+
+
+
+
+
+
--- python/find_nearest_three_restaurants.py
+++ python/find_nearest_three_restaurants.py
@@ -1,11 +1,11 @@
 import random
-CURRENT_POSITION = (50, 70)
-NO_RESTAURANT = 50 + 65
+CURRENT_POSITION = (50, 50)
+NO_RESTAURANT = 1000 + 65
 SIZE_OF_SIG = 100
 DISTANCE_RANGE = 50
 
 def find_nearest_three_restaurants(my_loc: tuple):
-    all_loc_list = generate_list() 
+    all_loc_list = generate_list()
     nearest_list, distance_list = [], []
     for loc in all_loc_list:
         dist = distance(my_loc, loc)
@@ -36,7 +36,7 @@ def findMin(list: list):
 
 def distance(loc1: tuple, loc2: dict):
     loc2_list = [key for key in loc2.keys()]
-    distance = sqrt(square(loc1[0] - loc2_list[0][0]) * square(loc1[1] - loc2_list[0][1]))
+    distance = sqrt(square(loc1[0] - loc2_list[0][0]) + square(loc1[1] - loc2_list[0][1]))
     return distance
 
 def square(x):
